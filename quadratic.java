6c4de2cd4313050fb87a63ede4a9eb0d7dbecf46
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2021-12-14 23:03:27 +0530

    Update quadratic.java

--- quadratic.java
+++ quadratic.java
@@ -1,30 +1,31 @@
 import java.util.*;
-public class quadratic{
-    public static void main(String args[]){
-        float a,b,c;
-        float root1, root2;
-        Scanner in = new Scanner(System.in);
-        System.out.println("\nValue of a");
-        a=in.nextInt();
-        System.out.println("\nValue of b");
-        b=in.nextInt();
-        System.out.println("\nValue of c");
-        c=in.nextInt();
-        float d = ((b*b)-(4*a*c));
 
-        if(d>0){
-            root1=((-b+(float)Math.sqrt(d))/(2*a));
-            root2=((-b-(float)Math.sqrt(d))/(2*a));
-            System.out.println("\nRoot 1 is: "+root1);
-            System.out.println("\nRoot 2 is: "+root2);
-        } else if (d==0){
-            root1=((-b+(float)Math.sqrt(d))/(2*a));
-            System.out.println("\nRoot 1 is: "+root1);
-            System.out.println("\nRoot 1 is: "+root1);
-        } else {
-            System.out.println("Roots are imaginary");
-        }
+public class quadratic {
+    public static void main(String args[]){
+       double a,b,c;
+       double r1,r2;
+       Scanner in = new Scanner(System.in);
+       System.out.println("Enter value of a");
+       a=in.nextDouble();
+       System.out.println("Enter value of b");
+       b=in.nextDouble();
+       System.out.println("Enter value of c");
+       c=in.nextDouble();
+       double d = ((b*b)-(4*a*c));
+
+       if(d>0)
+       {
+           r1 = ((-b+Math.sqrt(d))/(2*a));
+           r2 = ((-b-Math.sqrt(d))/(2*a));
+           System.out.println("Root 1 is "+r1);
+           System.out.println("Root 2 is "+r2);
+       } else if (d==0) {
+           r1=r2=(-b/(2*a));
+           System.out.println("Root 1 is "+r1);
+           System.out.println("Root 2 is "+r2);
+       } else {
+           System.out.println("roots are imaginary");
+       }
+
     }
 }
-
-
