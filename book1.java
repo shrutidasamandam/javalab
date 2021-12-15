616cbd6948e5a65c27f60b09122effba288e47f3
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2021-12-15 07:49:06 +0530

    Update book1.java

--- book1.java
+++ book1.java
@@ -1,54 +1,64 @@
-import java.util.Scanner;
-class book{
-    String name;
+/* Create a class
+Book which contains four members: name, author, price, num_pages. Include a
+constructor to set the values for the members. Include methods to set and get
+the details of the objects. Include a toString( ) method that could display
+the complete details of the book. Develop a Java program to create n book
+objects.*/
+
+
+import java.util.*;
+
+class book {
+    String name; 
     String author;
     double price;
     int num_pages;
+
     book(){
     }
-    book(String name, String author, int price, int num_pages){
+
+    book(String name, String author, double price, int num_pages){
         this.name=name;
-        this.num_pages = num_pages;
-        this.author = author;
-        this.price = price;
+        this.author=author;
+        this.price=price;
+        this.num_pages=num_pages;
     }
 
     void accept(){
-        Scanner in = new Scanner (System.in);
-        System.out.println("\nEnter name of the book");
-        name=in.nextLine();
-        System.out.println("\nEnter author of the book");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter name of book");
+        name = in.nextLine();
+        System.out.println("Enter author of book");
         author=in.nextLine();
-        System.out.println("\nEnter price of the book");
+        System.out.println("Enter price of book");
         price=in.nextDouble();
-        System.out.println("\nEnter number of pages of the book");
+        System.out.println("Enter pages of book");
         num_pages=in.nextInt();
-
     }
 
     public String toString()
     {
-        return("\nname: " +name+ "\nauthor: " +author+ "\nprice: " +price+ "\nnumber of pages: " +num_pages);
-
+        return("\nname: "+name+"\nauthor: "+author+"\nprice: "+price+ "\npages: "+num_pages);
     }
 }
 
 class book1{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        book b1 = new book("Height","Anne",299,345);
-        System.out.println("\nconstructor values: "+b1);
-        System.out.println("\nenter number of books");
+        book b1 = new book ("Height","Anne",299,345);
+        System.out.println("Constructor values are: "+b1);
+        System.out.println("Enter number of books");
         int n = in.nextInt();
         book b[] = new book[n];
         for(int i=0;i<n;i++){
             b[i]=new book();
-            System.out.println("\nenter the details of the "+(i+1)+ "book");
+            System.out.println("Enter the details of the "+(i+1)+ "book");
             b[i].accept();
         }
         for(int i=0;i<n;i++){
-            System.out.println("\ndetails of the book"+(i+1));
+            System.out.println("Details of the book "+(i+1));
             System.out.println(b[i]);
         }
+
     }
 }
