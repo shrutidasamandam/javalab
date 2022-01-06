468df167dffb0c8fe8b6a56a51deb679a663b718
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2022-01-06 11:11:39 +0530

    Create exceptions.java

---
+++ exceptions.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+class fatherAgeException extends Exception
+{
+public String toString()
+{
+return ("Father's age is less than 0.");
+}
+}
+
+class sonAgeException extends Exception
+{
+int a;
+sonAgeException (int age)
+{
+a = age;
+}
+public String toString()
+{
+if (a < 0)
+return ("Son's age is less than 0.");
+else
+return ("Son's age is more than father's age.");
+}
+}
+
+class Father
+{
+public int age1;
+Scanner scan = new Scanner (System.in);
+Father()
+{
+System.out.println ("Enter father's age: ");
+age1 = scan.nextInt();
+}
+void ex1 () throws fatherAgeException
+{
+if (age1 < 0)
+throw new fatherAgeException();
+}
+}
+
+class Son extends Father
+{
+public int age2;
+Son()
+{
+System.out.print ("Enter son's age: ");
+age2 = scan.nextInt();
+}
+void ex2() throws sonAgeException
+{
+if (age2 < 0 || age2 > super.age1)
+throw new sonAgeException (age2);
+}
+}
+
+class exceptions
+{
+public static void main (String[] args)
+{
+Son s = new Son();
+{
+try
+{
+s.ex1();
+}
+catch (fatherAgeException e)
+{
+System.out.println (e);
+}
+try
+{
+s.ex2();
+}
+catch (sonAgeException e)
+{
+System.out.println (e);
+}
+}
+}
+}
