fb54f9cded12cc8b7736b27411bf93283ab556a1
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2022-02-10 22:39:46 +0530

    Update exceptions.java

--- exceptions.java
+++ exceptions.java
@@ -1,81 +1,76 @@
 import java.util.Scanner;
-class fatherAgeException extends Exception
-{
-public String toString()
-{
-return ("Father's age is less than 0.");
-}
+
+class fatherAgeException extends Exception{
+    public String toString()
+    {
+        return ("Father's age is less than 0");
+    }
 }
 
-class sonAgeException extends Exception
-{
-int a;
-sonAgeException (int age)
-{
-a = age;
-}
-public String toString()
-{
-if (a < 0)
-return ("Son's age is less than 0.");
-else
-return ("Son's age is more than father's age.");
-}
+class sonAgeException extends Exception{
+    int a;
+    
+    sonAgeException(int age)
+    {
+        a=age;
+    }
+
+    public String toString()
+    {
+        if(a<0){
+            return ("Son's age is less than 0");
+        } else {
+            return ("Son's age is more than fathers age");
+        }
+    }
 }
 
-class Father
-{
-public int age1;
-Scanner scan = new Scanner (System.in);
-Father()
-{
-System.out.println ("Enter father's age: ");
-age1 = scan.nextInt();
-}
-void ex1 () throws fatherAgeException
-{
-if (age1 < 0)
-throw new fatherAgeException();
-}
+class Father {
+    public int age1;
+    Scanner in = new Scanner(System.in);
+
+    Father()
+    {
+        System.out.println("Enter Father's age");
+        age1 = in.nextInt();
+    }
+
+    void ex1() throws fatherAgeException
+    {
+        if(age1<0){
+            throw new fatherAgeException();
+        }
+    }
 }
 
-class Son extends Father
-{
-public int age2;
-Son()
-{
-System.out.print ("Enter son's age: ");
-age2 = scan.nextInt();
-}
-void ex2() throws sonAgeException
-{
-if (age2 < 0 || age2 > super.age1)
-throw new sonAgeException (age2);
-}
+class Son extends Father {
+    public int age2;
+    
+    Son()
+    {
+        System.out.println("Enter Son's age");
+        age2 = in.nextInt();
+    }
+
+    void ex2() throws sonAgeException
+    {
+        if(age2<0||age2>super.age1){
+            throw new sonAgeException(age2);
+        }
+    }
 }
 
-class exceptions
-{
-public static void main (String[] args)
-{
-Son s = new Son();
-{
-try
-{
-s.ex1();
-}
-catch (fatherAgeException e)
-{
-System.out.println (e);
-}
-try
-{
-s.ex2();
-}
-catch (sonAgeException e)
-{
-System.out.println (e);
-}
-}
+class Exceptions{
+    public static void main(String args[]){
+        Son s = new Son();
+        try{
+            s.ex1();
+        }catch (fatherAgeException e) {
+        System.out.println(e);
+    }try {
+        s.ex2();
+    } catch (sonAgeException e) {
+        System.out.println(e);
+    }
 }
 }
